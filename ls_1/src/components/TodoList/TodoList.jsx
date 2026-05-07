b858d29fe826715864039dcdcb73ee67a3e05119
Mukhammad <muxammed.toktogulov@gmail.com>
2026-05-07 16:00:11 +0600

    HW3

--- ls_1/src/components/TodoList/TodoList.jsx
+++ ls_1/src/components/TodoList/TodoList.jsx
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addTodo, deleteTodo, toggleTodo } from '../../store/counter-slice';
 import './style.css';
+import { Button } from 'antd';
+import { Input } from 'antd';
+import { Checkbox } from 'antd';
 const TodoList = () => {
 	const [text, setText] = useState('');
 	const todos = useSelector((state) => state.todoList.todo);
 	const dispatch = useDispatch();
+
 	// const count = useSelector((state) => state.counter.count);
 	const add_todo_btn = () => {
 		if (text.trim() === '') return;
@@ -17,26 +21,36 @@ const TodoList = () => {
 	return (
 		<div>
 			<div className="todo">
-				<input
+				<Input
+					style={{ width: '300px' }}
 					type="text"
 					placeholder="Введите задачу"
 					value={text}
 					onChange={(e) => setText(e.target.value)}
 				/>
-				<button onClick={add_todo_btn}>Добавить задачу</button>
+				<Button onClick={add_todo_btn}>Добавить задачу</Button>
 			</div>
 			<ul>
 				{todos.map((item, index) => (
-					<li key={item.id}>
-						{item.text}
-						<button onClick={() => dispatch(deleteTodo(item.id))}>
-							Удалить
-						</button>
-						<input
-							type="checkbox"
+					<li
+						key={item.id}
+						style={{
+							width: '450px',
+							display: 'flex',
+							justifyContent: 'space-between',
+							alignItems: 'center',
+						}}
+					>
+						<Checkbox
 							checked={item.completed}
 							onChange={() => dispatch(toggleTodo(item.id))}
-						/>
+						>
+							{item.text}
+						</Checkbox>
+
+						<Button onClick={() => dispatch(deleteTodo(item.id))}>
+							Удалить
+						</Button>
 					</li>
 				))}
 			</ul>
